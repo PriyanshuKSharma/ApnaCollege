6e42e934c4fcb9f3cb7b887baa3b7b1ded74d791
PriyanshuKSharma <priyanshu17ks@gmail.com>
2024-12-29 13:55:43 +0530

    Added Hollow rectangle

---
+++ patterns2.java
@@ -0,0 +1,26 @@
+public class patterns2 {
+
+    public static void hollow_rectangle(int totRows, int totCols) {
+
+        //outer loop for rows
+        for(int i=1; i<=totRows; i++) {
+
+            //inner loop for columns
+            for(int j=1; j<=totCols; j++) {
+
+                //if i is 1 or i is totRows or j is 1 or j is totCols(outer boundary)(cell - (i,j))
+                if(i==1 || i==totRows || j==1 || j==totCols) {
+                    //boundary cells
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        hollow_rectangle(5, 6);
+    }
+    
+}

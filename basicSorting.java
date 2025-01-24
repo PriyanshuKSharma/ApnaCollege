29247ebc022cfa62a349000da6a3882d90a7ed37
PriyanshuKSharma <priyanshu17ks@gmail.com>
2025-01-24 21:28:12 +0530

    Added insertion sort, in-built sorting and counting sort

--- basicSorting.java
+++ basicSorting.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class basicSorting{
 
     public static void bubbleSort(int arr[]) {
@@ -35,6 +36,46 @@ public class basicSorting{
         }
     }
 
+    public static void insertionSort(int arr[]) {
+        for(int i=1; i<arr.length; i++) {
+            int curr = arr[i];
+            int prev = i-1;
+            //finding out the correct position to insert
+            while(prev >= 0 && arr[prev] > curr) {
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            //insertion
+            arr[prev+1] = curr;
+        }
+    }
+
+    public static void  inBuiltSort(int arr[]) {
+        Arrays.sort(arr, 0, arr.length);
+    }
+
+    public static void countingSort(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+
+        int count[] = new int[largest+1];
+        for(int i=0; i<arr.length; i++) {
+            count[arr[i]]++;
+        }
+
+        //sorting
+        int j=0;
+        for(int i=0; i<count.length; i++) {
+            while(count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
     public static void printArray(int arr[]) {
         for(int i=0; i<arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -46,9 +87,14 @@ public class basicSorting{
 
     public static void main(String[] args) {
         int arr[] = {5,4,1,3,2};
-        bubbleSort(arr);
-        selectionSort(arr);
-        printArray(arr);
+        //bubbleSort(arr);
+        //selectionSort(arr);
+        //insertionSort(arr);
+        //inBuiltSort(arr);
+        //inBuiltSort(arr);
+
+        //Arrays.sort(arr, Collections.reverseOrder());
+        //printArray(arr);
 
     }
 }
